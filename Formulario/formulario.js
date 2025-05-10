3d450fa3c2bc6e7be853aa68d09ddce8276b9962
RaulFerna <raulfernandesnetto@gmail.com>
2025-05-09 21:57:27 -0300

    Anotações importantes

--- Formulario/formulario.js
+++ Formulario/formulario.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('DOMContentLoaded', function(){
     var form = document.getElementById('login');
 
     form.addEventListener('submit' , function (event){
         event.preventDefault();
-        const formData = new FormData(form);
-        const user = Object.fromEntries(formData.entries());
+        const formData = new FormData(form);// transforma em objeto para conseguir extrair os dados 
+        const user = Object.fromEntries(formData.entries());// extraindo os dados.
         console.log(user);
         let textoSaida = `Olá ${user.nome}!`;
         alert(textoSaida);
